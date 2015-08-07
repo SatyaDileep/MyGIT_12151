73733ed155336e6838b4df11e9a370030c78d903
SatyaDileep <SatyaDileep@users.noreply.github.com>
2015-08-07 13:20:22 +0530

    Update AgeComparatorOnEmployee.java
    Relocated the documented changes

--- ComparatorDemo/src/com/pramati/comparatorDemo/AgeComparatorOnEmployee.java
+++ ComparatorDemo/src/com/pramati/comparatorDemo/AgeComparatorOnEmployee.java
@@ -1,8 +1,8 @@
-package com.pramati.comparatorDemo;
-
-import java.util.Comparator;
 //This class implements comparator interface to provide the logic for the comparison on age, if needed on name or other attribute
 //you may have to write another class for that which is painful. Java 8 provides a solution for it.
+package com.pramati.comparatorDemo;
+import java.util.Comparator;
+
 public class AgeComparatorOnEmployee implements Comparator<Employee>{
 
 	@Override
